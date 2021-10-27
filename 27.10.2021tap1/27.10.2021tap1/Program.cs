6e6fb277b9d871eeee7ff95cc009b0608607c132
10hesen044 <tu201906250@code.edu.az>
2021-10-27 12:01:23 +0400

    project added

---
+++ 27.10.2021tap1/27.10.2021tap1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _27._10._2021tap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindIndex("salam", 'l');
+        }
+        static void FindIndex(string word, char character)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == character)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine(-1);
+            return;
+        }
+    }
+}
